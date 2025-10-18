559fd3c177f56051712d04b137e9d490cf946b0b
Stuti Porwal <porwalstuti01@gmail.com>
2025-10-18 20:59:12 +0530

    Update page.js

--- page.js
+++ page.js
@@ -1,1 +1,16 @@
+'use client'
+
+import BlogItem from "@/Components/BlogItem";
+import Header from "@/Components/Header";
+
+export default function Home() {
+  return (
+    <>
+
+      <Header/>
+      <BlogItem/>
+    
+    </>
+  );
+}
 
