53587fd203e50e302a934ed2c75d32054e1e7239
Stuti Porwal <porwalstuti01@gmail.com>
2025-10-24 18:28:53 +0530

    Update Footer.jsx

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -1,1 +1,17 @@
+import React from 'react'
 
+const Footer = () => {
+    return (
+        <div className='flex justify-around flex-col gap-2 sm:gap-0 sm:flex-row bg-black py-5 items-center'>
+            <Image src={assets.logo_light} alt=' ' width ={120} />
+            <p className='text-sm text-white'>All rights reserved. Copyright @blogger</p>
+            <div className='flex'>
+                <Image src={assets.facebook_icon} alt=' ' widht={40} />
+                <Image src={assets.twitter_icon} alt=' ' widht={40} />
+                <Image src={assets.googleplus_icon} alt=' ' widht={40} />
+            </div>
+        </div>
+    )
+}
+
+export default Footer
