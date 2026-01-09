ebf87f3efeb83ca06f50cdca12608b7aa8c80453
Stuti Porwal <porwalstuti01@gmail.com>
2026-01-09 11:53:14 +0530

    added default function footer

--- app/layout.js
+++ app/layout.js
@@ -73,3 +73,10 @@ export default function Navbar() {
   );
 }
 
+export default function Footer() {
+  return (
+    <footer className="border-t py-4 text-center text-sm text-gray-500">
+      © {new Date().getFullYear()} Blog App. All rights reserved.
+    </footer>
+  );
+}
